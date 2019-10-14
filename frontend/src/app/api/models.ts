fa68f37091dd1e7797fe5089d1d3be72cdfca913
mBrossois <mark.dnb@live.nl>
2019-10-14 15:15:13 +0200

    Restructured FE (#5)
    * Setup angular
    
    * Added qr scanner
    
    * Changed style of qr code scanner
    
    * Changed style of qr code scanner2
    
    * Restructured frontend

---
+++ frontend/src/app/api/models.ts
@@ -0,0 +1,4 @@
+export { Achievement } from './models/achievement';
+export { ModelAndView } from './models/model-and-view';
+export { Ninja } from './models/ninja';
+export { View } from './models/view';
---
+++ frontend/src/app/api/services.ts
@@ -0,0 +1,2 @@
+export { BasicErrorControllerService } from './services/basic-error-controller.service';
+export { NinjaResourceService } from './services/ninja-resource.service';
