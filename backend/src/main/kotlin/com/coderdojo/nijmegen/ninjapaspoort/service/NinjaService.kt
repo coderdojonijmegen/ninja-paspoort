90a0312c4be7c09ef5b6c7f5479d6f831d62d865
Kevin van den Broek <kevin@Kevins-MacBook-Pro.local>
2019-10-14 15:57:43 +0200

    Always create ninja when ID is not present

--- backend/src/main/kotlin/com/coderdojo/nijmegen/ninjapaspoort/service/NinjaService.kt
+++ backend/src/main/kotlin/com/coderdojo/nijmegen/ninjapaspoort/service/NinjaService.kt
@@ -3,6 +3,7 @@ package com.coderdojo.nijmegen.ninjapaspoort.service
 import com.coderdojo.nijmegen.ninjapaspoort.repository.NinjaRepository
 import com.coderdojo.nijmegen.ninjapaspoort.service.model.Achievement
 import com.coderdojo.nijmegen.ninjapaspoort.service.model.Ninja
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import java.util.*
 
@@ -11,7 +12,7 @@ class NinjaService(val ninjaRepository: NinjaRepository, val achievementService:
 
     fun insertNinja(ninja: Ninja) = ninjaRepository.save(ninja)
 
-    fun retrieveNinja(id: UUID): Ninja = ninjaRepository.getOne(id)
+    fun retrieveNinja(id: UUID): Ninja = ninjaRepository.findByIdOrNull(id) ?: ninjaRepository.save(Ninja(id))
 
     fun addAchievement(ninjaId: UUID, achievement: Achievement) {
         val savedAchievement = achievementService.insertAchievement(achievement)
