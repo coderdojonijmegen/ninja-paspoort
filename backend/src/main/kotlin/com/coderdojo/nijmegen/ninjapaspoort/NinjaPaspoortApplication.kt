f7524cd02d22bcb9d6bab5018e3eb08fb1107fc8
Marvin <marvin.zwolsman@gmail.com>
2019-10-14 11:52:34 +0200

    Add docker build support (#1)
    * Initial commit
    
    * Add Dockerfile
    
    * .gitignore fix
    
    * Add root .gitignore
    
    * Add docker-compose

---
+++ backend/src/main/kotlin/com/coderdojo/nijmegen/ninjapaspoort/NinjaPaspoortApplication.kt
@@ -0,0 +1,11 @@
+package com.coderdojo.nijmegen.ninjapaspoort
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NinjaPaspoortApplication
+
+fun main(args: Array<String>) {
+	runApplication<NinjaPaspoortApplication>(*args)
+}
